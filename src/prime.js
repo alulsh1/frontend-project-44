940df2d706d00868fd1656c8bcc46a2fa5a843a9
Aleksandr Ulshin <aleksandr.ulshin@rhsolutions.local>
2023-04-26 14:16:22 +0300

    222

--- src/prime.js
+++ src/prime.js
@@ -4,7 +4,7 @@ import getRandomNumber from './random.js';
 const navH1 = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function isProme(num) {
-  for (let i = 2; i < Math.sqrt(num); i += 1) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return 'no';
     }
